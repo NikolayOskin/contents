5fbed75ac414cb17029ff198d33753a9fcd0cc61
Oskin <wskeal@gmail.com>
2019-05-20 11:57:46 +0300

    updating view

--- src/views/table.blade.php
+++ src/views/table.blade.php
@@ -2,7 +2,7 @@
     @foreach($contents as $key => $header)
         <li><a href="#header-{{$header['count']}}">{{$header['header']}}</a></li>
         @if (!empty($header['childs']))
-            @include('vendor.contents.contents', ['contents' => $header['childs']])
+            @include('nikolay-oskin.contents.table', ['contents' => $header['childs']])
         @endif
     @endforeach
 </ul>
