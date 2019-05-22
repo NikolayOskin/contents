e0811107f08143e4e59a5fc53e175b835e952f27
Oskin <wskeal@gmail.com>
2019-05-22 22:13:17 +0300

    updating tests

---
+++ testdata/texts.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    'smallText' => '
+    <h2>First h2</h2>
+    <p>Some text</p>    
+    <h3>First h3</h3>
+    <p>Some text</p>    
+    <h3>Second h3</h3>
+    <p>Some text</p>    
+    <h4>First h4</h4>
+    <p>Some text</p>   
+    ',
+    'bigText' => '
+    <h2>First h2</h2>
+    <p>Some text</p>    
+    <h3>First h3</h3>
+    <p>Some text</p>    
+    <h3>Second h3</h3>
+    <p>Some text</p>    
+    <h4>First h4</h4>
+    <p>Some text</p>    
+    <h4>Second h4</h4>
+    <p>Some text</p>    
+    <h4>Again h4</h4>
+    <p>Some text</p>    
+    <h3>Once again h3</h3>
+    <p>Some text</p>    
+    <h4>New h4</h4>
+    <p>Some text</p>    
+    <h5>New h5</h5>
+    <p>Some text</p>    
+    <h5>Again f5</h5>
+    <p>Some text</p>    
+    <h6>New h6</h6>
+    <p>Some text</p>    
+    <h6>Again h6</h6>
+    <p>Some text</p>    
+    <h2>Latest header</h2>
+    <p>Some text</p>
+    ',
+    'handledBigText' => '
+    <h2 id="header-1">First h2</h2>
+    <p>Some text</p>    
+    <h3 id="header-2">First h3</h3>
+    <p>Some text</p>    
+    <h3 id="header-3">Second h3</h3>
+    <p>Some text</p>    
+    <h4 id="header-4">First h4</h4>
+    <p>Some text</p>    
+    <h4 id="header-5">Second h4</h4>
+    <p>Some text</p>    
+    <h4 id="header-6">Again h4</h4>
+    <p>Some text</p>    
+    <h3 id="header-7">Once again h3</h3>
+    <p>Some text</p>    
+    <h4 id="header-8">New h4</h4>
+    <p>Some text</p>    
+    <h5 id="header-9">New h5</h5>
+    <p>Some text</p>    
+    <h5 id="header-10">Again f5</h5>
+    <p>Some text</p>    
+    <h6 id="header-11">New h6</h6>
+    <p>Some text</p>    
+    <h6 id="header-12">Again h6</h6>
+    <p>Some text</p>    
+    <h2 id="header-13">Latest header</h2>
+    <p>Some text</p>
+    '
+];
--- tests/TableOfContentsTest.php
+++ tests/TableOfContentsTest.php
@@ -9,22 +9,35 @@ class TableOfContentsTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
         $str = 'Test';
-        new Contents($str, ['h1', 'h2', 'h15']);
+        $contents = new Contents();
+        $contents->fromText($str)->setTags(['h1', 'h2', 'h15']);
     }
 
     public function test_one_of_tags_array_element_has_incorrect_type_exception()
     {
         $this->expectException(InvalidArgumentException::class);
         $str = 'Test';
-        new Contents($str, ['h1', 'h2', 15]);
+        $contents = new Contents();
+        $contents->fromText($str)->setTags(['h1', 'h2', 15]);
     }
 
     public function test_returned_text_equals_passed_if_it_less_than_minlength()
     {
         $str = 'Passed text';
         $minLength = 1500;
-        $tags = ['h2', 'h3'];
-        $contents = new Contents($str, $tags, $minLength);
+        $tags = ['h2', 'h3', 'h4', 'h5'];
+        $contents = new Contents();
+        $contents->fromText($str)->setTags($tags)->setMinLength($minLength);
         $this->assertEquals($str, $contents->getHandledText());
+    }
+
+    public function test_id_attributes_are_properly_added_to_text_headers()
+    {
+        $textsArray = include __DIR__ . './../testdata/texts.php';
+        $inputText = $textsArray['bigText'];
+        $outputText = $textsArray['handledBigText'];
+        $contents = new Contents();
+        $contents->fromText($inputText)->setMinLength(100);
+        $this->assertEquals($outputText, $contents->getHandledText());
     }
 }
